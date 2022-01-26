808deb04cb93dfa167f2409a8ff63b008d9bf88e
Sarah Nicholson <sarah.nicholson.13@ucl.ac.uk>
2022-01-26 18:05:02 +0000

    setting headless mode for chrome for selenium tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -109,11 +109,11 @@ def chrome_driver(request):
         For running on your computer: `headless` to be commented out and `window-size` not
     """
     options = ChromeOptions()
-    # options.add_argument("--headless")
+    options.add_argument("--headless") # use for GitHub Actions CI
+    # options.add_argument("--window-size=1920,1080")  # use when not in headless
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1920,1080")  # use only when not in headless
     chrome_driver = Chrome(options=options)
     request.cls.driver = chrome_driver
     yield
